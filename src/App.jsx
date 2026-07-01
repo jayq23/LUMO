9561555c824f43e987bde62c6dc5871c3c878c08
Jay <jaysorreda4@gmail.com>
2026-07-01 18:44:12 +0800

    Add new route for new panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from './auth/AuthContext.jsx'
-import AIAssistant from './frontend/AIAssistant.jsx'
 import Login from './auth/login.jsx'
 import Register from './auth/register.jsx'
 import Dashboard from './frontend/dashboard.jsx'
@@ -9,6 +8,7 @@ import Settings from './frontend/settings.jsx'
 import Budgets from './frontend/budgets.jsx'
 import Report from './frontend/report.jsx'
 import Transactions from './frontend/transaction.jsx'
+import Assistant from './frontend/assistant.jsx'
 import { useEffect } from "react"; 
 
 function App() {
@@ -27,10 +27,10 @@ function App() {
           <Route path="/budgets" element={<Budgets />} />
           <Route path="/reports" element={<Report />} />
           <Route path="/transactions" element={<Transactions />} />
+          <Route path="/assistant" element={<Assistant />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
-        <AIAssistant />
       </BrowserRouter>
     </AuthProvider>
   )
